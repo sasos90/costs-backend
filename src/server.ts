91664117307df713a1caf41b9266528d3652292d
Saso Sabotin <sasos90@gmail.com>
2018-04-05 06:48:44 +0200

    Tslint update + cost route created

--- src/server.ts
+++ src/server.ts
@@ -1,43 +1,45 @@
 /**
  * Module dependencies.
  */
-import * as bodyParser from "body-parser";
-import * as compression from "compression";  // compresses requests
-import * as mongo from "connect-mongo"; // (session)
-import * as dotenv from "dotenv";
-import * as errorHandler from "errorhandler";
-import * as express from "express";
-import * as flash from "express-flash";
-import * as session from "express-session";
-import * as lusca from "lusca";
-import * as mongoose from "mongoose";
-import * as logger from "morgan";
-import * as passport from "passport";
-import * as path from "path";
-import expressValidator = require("express-validator");
+import * as bodyParser from 'body-parser';
+import * as compression from 'compression';  // compresses requests
+import * as mongo from 'connect-mongo'; // (session)
+import * as dotenv from 'dotenv';
+import * as errorHandler from 'errorhandler';
+import * as express from 'express';
+import * as flash from 'express-flash';
+import * as session from 'express-session';
+import * as lusca from 'lusca';
+import * as mongoose from 'mongoose';
+import * as logger from 'morgan';
+import * as passport from 'passport';
+import * as path from 'path';
+import expressValidator = require('express-validator');
 
 /**
  * Load environment variables from .env file, where API keys and passwords are configured.
  */
-dotenv.config({ path: ".env.example" });
+dotenv.config({ path: '.env.example' });
 
 /**
  * Routes
  */
-import accountRouter from "./routes/account";
-import apiRouter from "./routes/api";
-import contactRouter from "./routes/contact";
-import oauthRouter from "./routes/oauth";
-import rootRouter from "./routes/root";
+import accountRouter from './routes/account';
+import apiRouter from './routes/api';
+import contactRouter from './routes/contact';
+import oauthRouter from './routes/oauth';
+import rootRouter from './routes/root';
 
 /**
  * API keys and Passport configuration.
  */
-import * as passportConfig from "./config/passport";
+import * as passportConfig from './config/passport';
+import {Router} from 'express';
 class App {
 
   // ref to Express instance
   public express: express.Application;
+  public router: Router;
   private readonly MongoStore = mongo(session);
 
   constructor() {
@@ -48,11 +50,11 @@ class App {
 
   }
   private middleware(): void {
-    this.express.set("port", process.env.PORT || 3000);
-    this.express.set("views", path.join(__dirname, "../views"));
-    this.express.set("view engine", "pug");
+    this.express.set('port', process.env.PORT || 3000);
+    this.express.set('views', path.join(__dirname, '../views'));
+    this.express.set('view engine', 'pug');
     this.express.use(compression());
-    this.express.use(logger("dev"));
+    this.express.use(logger('dev'));
     this.express.use(bodyParser.json());
     this.express.use(bodyParser.urlencoded({ extended: true }));
     this.express.use(expressValidator());
@@ -68,7 +70,7 @@ class App {
     this.express.use(passport.initialize());
     this.express.use(passport.session());
     this.express.use(flash());
-    this.express.use(lusca.xframe("SAMEORIGIN"));
+    this.express.use(lusca.xframe('SAMEORIGIN'));
     this.express.use(lusca.xssProtection(true));
     this.express.use((req, res, next) => {
       res.locals.user = req.user;
@@ -77,37 +79,40 @@ class App {
     this.express.use((req, res, next) => {
       // After successful login, redirect back to the intended page
       if (!req.user &&
-        req.path !== "/login" &&
-        req.path !== "/signup" &&
+        req.path !== '/login' &&
+        req.path !== '/signup' &&
         !req.path.match(/^\/auth/) &&
         !req.path.match(/\./)) {
         req.session.returnTo = req.path;
       } else if (req.user &&
-        req.path === "/account") {
+        req.path === '/account') {
         req.session.returnTo = req.path;
       }
       next();
     });
-    this.express.use(express.static(path.join(__dirname, "public"), { maxAge: 31557600000 }));
+    this.express.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
   }
   /**
    * Primary app routes.
    */
   private routes(): void {
-    this.express.use("/", rootRouter);
-    this.express.use("/api", apiRouter);
-    this.express.use("/auth", oauthRouter);
-    this.express.use("/account", accountRouter);
-    this.express.use("/contact", contactRouter);
+    this.express.use('/', rootRouter);
+    this.express.use('/api', apiRouter);
+    this.express.use('/auth', oauthRouter);
+    this.express.use('/account', accountRouter);
+    this.express.use('/contact', contactRouter);
+    this.express.delete('/cost', (req, res, next) => {
+      res.json('works');
+    });
   }
 
   private launchConf() {
     // mongoose.Promise = global.Promise;
     mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI);
 
-    mongoose.connection.on("error", () => {
+    mongoose.connection.on('error', () => {
       // tslint:disable-next-line:no-console
-      console.log("MongoDB connection error. Please make sure MongoDB is running.");
+      console.log('MongoDB connection error. Please make sure MongoDB is running.');
       process.exit();
     });
 
@@ -116,12 +121,12 @@ class App {
     /**
      * Start Express server.
      */
-    this.express.listen(this.express.get("port"), () => {
+    this.express.listen(this.express.get('port'), () => {
       // tslint:disable-next-line:no-console
-      console.log(("  App is running at http://localhost:%d \
-      in %s mode"), this.express.get("port"), this.express.get("env"));
+      console.log(('  App is running at http://localhost:%d \
+      in %s mode'), this.express.get('port'), this.express.get('env'));
       // tslint:disable-next-line:no-console
-      console.log("  Press CTRL-C to stop\n");
+      console.log('  Press CTRL-C to stop\n');
     });
   }
 }
