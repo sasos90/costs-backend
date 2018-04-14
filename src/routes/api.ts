2dbe14368353d8c3e56db721a67be1ffba32a003
Saso Sabotin <sasos90@gmail.com>
2018-04-14 21:31:31 +0200

    Verifying with JWT token

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,6 +1,9 @@
 import { NextFunction, Request, Response, Router } from 'express';
 import * as apiController from '../controllers/api';
 import * as winston from 'winston';
+import { JsonWebTokenError, NotBeforeError, TokenExpiredError, verify, VerifyCallback } from 'jsonwebtoken';
+import * as HttpStatus from 'http-status-codes';
+import { ResponseMsg } from '../../helper/response-msg';
 
 class Api {
   public router: Router;
@@ -12,7 +15,21 @@ class Api {
     this.router.use((req: Request, res: Response, next: NextFunction) => {
       winston.info('Authentication');
       const token = req.body.token || req.query.token || req.headers['x-access-token'];
-      next();
+      if (token && token !== '') {
+        // Verify the JWT
+        return verify(token, process.env.JWT_SECRET,
+          (err: JsonWebTokenError | NotBeforeError | TokenExpiredError, decoded: object | string) => {
+          if (err) {
+            winston.error('Token verifying failed', err);
+            return res.status(HttpStatus.BAD_REQUEST).json(ResponseMsg.error('Failed to authenticate token!'));
+          } else {
+            // Store decoded data to locals user.
+            (res as any).locals.user = decoded;
+            return next();
+          }
+        });
+      }
+      return res.status(HttpStatus.BAD_REQUEST).json(ResponseMsg.error(`'token' is missing in request.`));
     });
     this.router.get('/', apiController.getApi);
 
